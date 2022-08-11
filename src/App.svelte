6a205fc2336efaf8f90d8d478419a7f5f7715f1b
Tanvir Chahal <tchahal@tiercon.com>
2022-08-11 08:41:57 -0400

    basic svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Child from './Child.svelte'
+	let name = 'Svelte';
+	let htmlString = '<strong>Something</strong> strong'
+</script>
+
+<h1>Testing {name}!</h1>
+
+
+<p>{@html htmlString}</p>
+<Child />
+
+<style>
+	h1 {
+		font-size:1em;
+	}
+</style>
---
+++ src/Child.svelte
@@ -0,0 +1,1 @@
+<h1>This is a child component</h1>
