2530905b323460efa02a9cdf8ee8acd9e254b8e3
Tanvir Chahal <tnvr009@gmail.com>
2022-08-15 09:02:45 -0400

    dom events svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
 	import ConditionalLogic from './ConditionalLogic.svelte'
 	import Each from './Each.svelte'
 	import AwaitBlock from './AwaitBlock.svelte'
+	import Dom from './Dom.svelte'
 
 	let name = 'Svelte';
 	let htmlString = '<strong>Something</strong> strong'
@@ -15,8 +16,13 @@
 		age: 999,
 		profession: 'Software'
 	}
-</script>
 
+	let userName = ''
+	const handleClick = (event) => {
+		console.log(event.detail.name)
+	}
+</script>
+<!-- 
 <h1>Testing {name}!</h1>
 
 
@@ -29,7 +35,8 @@
 <Props {...person}  age= {42000}/>
 <ConditionalLogic />
 <Each />
-<AwaitBlock />
+<AwaitBlock /> -->
+<Dom on:message={handleClick}/>
 
 <style>
 	h1 {
---
+++ src/Dom.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher()
+
+    const moveUp = () => {
+        dispatch('message',{
+            age: 27,
+            name: 'Tanvir'
+        })
+    }
+</script>
+
+<button on:click={moveUp}>Dispatch Action</button>
