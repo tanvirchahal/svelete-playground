7d453ce43a4921d0b3b93829406b5198904eb80f
Tanvir Chahal <tnvr009@gmail.com>
2022-08-23 09:23:24 -0400

    setup ssr practice app using svelte-kit

---
+++ ssr-app/src/routes/cars/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    let response = ''
+    let loading = true
+    const getCars = async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const cars = await data.json()
+        return cars
+    }
+
+    getCars().then(data => {
+        response = data
+        loading = false;
+    })
+    
+</script>
+
+{#if loading}
+ loading......
+{/if}
+{#if response.length > 1}
+<h1>List of Cars</h1>
+<p>List of Cars: </p>
+<ul>
+    {#each response as car}
+        <li>{car.name}</li>
+    {/each}
+</ul>
+{/if}
