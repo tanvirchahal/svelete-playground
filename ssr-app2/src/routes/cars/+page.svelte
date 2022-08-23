a17daebbd715a3d836a83b3594d7e0d5ad7baef7
Tanvir Chahal <tnvr009@gmail.com>
2022-08-23 10:20:06 -0400

    wip svelte ssr using sveltekit

---
+++ ssr-app2/src/routes/cars/+page.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    export async function load({fetch}) {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const cars = await data.json()
+        return {
+            props: {
+                cars
+            }
+        }
+    }
+</script>
+<script>
+    export let cars
+</script>
+{ cars }
+<!-- <p>List of Cars: </p>
+<ul>
+    {#each cars as car}
+        <li>{car.name}</li>
+    {/each}
+</ul> -->
